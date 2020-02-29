beb0c1c2ef27a9c5423de20b02b94da73f9fd612
emat <57556347+tonysyz@users.noreply.github.com>
2020-03-01 05:47:26 +0800

    Update 3_functions_in_test.py

--- 3_functions_in_test.py
+++ 3_functions_in_test.py
@@ -30,13 +30,16 @@ class Func(GraphScene):
             Write(text6),
             Write(text7),
         )
+        self.wait()
         group = VGroup(text2,text3,text4,text5,text6,text7)
         self.wait(3)
         text8 = TextMobject("那么,这些函数在压轴题中有什么表现呢?").next_to(text1,DOWN*2)
         self.wait(2)
         self.play(ReplacementTransform(group,text8))
         text9 = TextMobject("来看一道高考数学导数压轴题").next_to(text8,DOWN*2)
+        self.wait(2)
         self.play(Write(text9))
+        self.wait(2)
         self.play(
             FadeOut(text9),
             FadeOut(text8),
@@ -51,7 +54,9 @@ class Func(GraphScene):
             Write(t2),
             Write(t3)
         )
+        self.wait(15)
         self.play(FadeOut(t3))
+        self.wait()
         t4 = TextMobject(
             r"$f(1)=$",
             r"$e(1-1)+2$\\",
@@ -59,6 +64,7 @@ class Func(GraphScene):
         ).next_to(t2,DOWN)
         t4[2].shift(LEFT).shift(DOWN)
         self.play(Write(t4))
+        self.wait()
         t5 = TextMobject(r"2").next_to(t4[0],RIGHT)
         t6 = TexMobject(
             r"f(1)=",
@@ -67,26 +73,38 @@ class Func(GraphScene):
         t7 = TexMobject(r"f'(x)={{{{ae^x}\over{x}}+{ae}^{x}\ln {x}}+ ",r"{{{bxe^{x-1}-be^{x-1}}\over {x^2}}}}").next_to(t4[2],RIGHT)
         t8 = TexMobject(r"f'(x)={{{{ae^x}\over{x}}+{ae}^{x}\ln {x}}+",r"{{{be}^{x-1}(x-1)}\over{x^2}}").next_to(t4[2],RIGHT)
         self.play(ReplacementTransform(t4[1],t5))
+        self.wait()
         self.play(Write(t6))
+        self.wait()
         self.play(Write(t7))
+        self.wait()
         self.play(ReplacementTransform(t7[1],t8[1]))
+        self.wait()
         t9 = TexMobject(r"\Rightarrow", r" a=1\\b=2").next_to(t8,DOWN)
         t9[0].shift(DOWN*0.45)
         self.play(Write(t9))
+        self.wait()
         group1 = VGroup(t2, t4[0], t5, t6, t4[2], t7[0], t8[1], t9)
         self.play(FadeOut(group1))
+        self.wait()
         t3.next_to(t1,DOWN).shift(LEFT*3.6)
         self.play(Write(t3))
+        self.wait()
         s1 = TexMobject(r"f(x)=", r"(", r"e^x\ln x + {{2e^{x-1}}\over {x}}", r")", r">1").next_to(t3,DOWN)
         s2 = TexMobject(r"\cdot", r"{{x}\over{e^x}}").next_to(s1[4],RIGHT)
         s3 = TexMobject(r"{{x}\over{e^x}}" ,r"\cdot").next_to(s1[1],LEFT)
         group2 = VGroup(s1[0], s1[2], s1[4])
         group3 = VGroup(s1[1], s1[3])
         self.play(Write(group2))
+        self.wait()
         self.play(FadeOut(s1[0]))
+        self.wait()
         self.play(Write(group3))
+        self.wait()
         self.play(Write(s2))
+        self.wait()
         self.play(Write(s3))
+        self.wait()
         s4 = TexMobject(r"{x\ln {x} +{{2}\over{e}}}", r" > {{x}\over {e^x}}")
         group4 = VGroup(s3, s1[2], s1[3], s1[1])
         group5 = VGroup(s2, s1[4])
@@ -94,6 +112,7 @@ class Func(GraphScene):
             ReplacementTransform(group4, s4[0]),
             ReplacementTransform(group5, s4[1])
         )
+        self.wait()
         s5 = TexMobject(
             r"h(x)=",
             r"x\ln x +{2 \over e}"
@@ -103,9 +122,13 @@ class Func(GraphScene):
             r"{x\over e^x}"
         ).move_to(np.array([-3.8,0,0]))
         self.play(Write(s5[0]))
+        self.wait()
         self.play(ReplacementTransform(s4[0], s5[1]))
+        self.wait()
         self.play(Write(s6[0]))
+        self.wait()
         self.play(ReplacementTransform(s4[1], s6[1]))
+        self.wait()
         s5_1 = TexMobject(
             r"h'(x)=",
             r"(x\ln x +{2 \over e})'"
@@ -115,56 +138,135 @@ class Func(GraphScene):
             r"({x\over e^x})'"
         ).move_to(np.array([-3.8,0,0]))
         self.play(ReplacementTransform(s5[0],s5_1[0]))
+        self.wait()
         self.play(ReplacementTransform(s6[0],s6_1[0]))
+        self.wait()
         self.play(ReplacementTransform(s5[1],s5_1[1]))
+        self.wait()
         self.play(ReplacementTransform(s6[1],s6_1[1]))
+        self.wait()
         s5_2 = TexMobject(
             r"h'(x)=",
             r"(x\ln x)' +({2 \over e})'"
         ).move_to(np.array([-3,1,0]))
         self.play(ReplacementTransform(s5_1[1],s5_2[1]))
+        self.wait()
         s5_3 = TexMobject(
             r"h'(x)=",
             r"(x\ln x)'",
             r"+{2 \over e}"
         ).move_to(np.array([-3,1,0]))
         self.play(ReplacementTransform(s5_2[1],s5_3[1]))
+        self.wait()
         s5_4 = TexMobject(
             r"h'(x)=",
             r"1+\ln x",
             r"+{2 \over e}"
         ).move_to(np.array([-3,1,0]))
         self.play(ReplacementTransform(s5_3[1], s5_4[1]))
+        self.wait()
         s6_2 = TexMobject(
             r"g'(x)=",
             r"{{1-x}\over {e^x}}"
         ).move_to(np.array([-3.8,0,0]))
         self.play(ReplacementTransform(s6_1[1], s6_2[1]))
+        self.wait()
         group6 = VGroup(t1, t3, s5_1[0], s6_1[0], s5_4[1], s6_2[1])
         self.play(FadeOut(group6))
         self.wait()
+
         self.setup_axes(animate=True)
         plane = NumberPlane()
         self.play(ShowCreation(plane))
         def ln(x):
             return math.log(x,e)
         hx = self.get_graph(lambda x: x * ln(x) + 2/e, x_min=0.02)
-        hx_label = self.get_graph_label(hx,label=r"h(x)=x \ln x + {2 \over e}")
+        hx_label = self.get_graph_label(hx,label=r"h(x)=x \ln x + {2 \over e}").shift(LEFT*5)
         h_x = self.get_graph(lambda x: ln(x)+1, x_min=0.02)
-        h_x_label = self.get_graph_label(h_x, label=r"h'(x)=\ln x + 1")
+        h_x_label = self.get_graph_label(h_x, label=r"h'(x)=\ln x + 1").shift(DOWN*2)
         gx = self.get_graph(lambda x: x/(e**x))
-        gx_label = self.get_graph_label(gx, label=r"g(x)={x \over {e^x}}")
+        gx_label = self.get_graph_label(gx, label=r"g(x)={x \over {e^x}}").move_to(np.array([-2,-1,0]))
         g_x = self.get_graph(lambda x:(x-1)/(e**x))
-        g_x_label = self.get_graph_label(g_x, label=r"g'(x)={{x-1}\over{e^x}}")
+        g_x_label = self.get_graph_label(g_x, label=r"g'(x)={{x-1}\over{e^x}}").shift(DOWN*0.7)
+        line1 = self.get_vertical_line_to_graph(1/e,hx)
+        dot1 = Dot().move_to(np.array([1/e, 1/e, 0]))
+        dot1_label = TexMobject(r"({1 \over e},{1 \over e})").next_to(dot1, UP).shift(LEFT*2)
+        line2 = self.get_vertical_line_to_graph(1, gx)
+        dot2 = Dot().move_to(np.array([1, 1/e, 0]))
+        dot2_label = TexMobject(r"(1, {1 \over e})").move_to(dot2, DOWN).shift(RIGHT*2)
         self.play(ShowCreation(hx))
+        self.wait()
         self.play(ShowCreation(hx_label))
+        self.wait()
         self.play(ShowCreation(h_x))
+        self.wait()
         self.play(ShowCreation(h_x_label))
+        self.wait()
+        self.play(Write(line1))
+        self.wait()
+        self.play(Write(dot1))
+        self.wait()
+        self.play(Write(dot1_label))
+        self.wait()
+        group7 = VGroup(hx, hx_label, h_x, h_x_label, line1, dot1, dot1_label)
+        self.play(FadeOut(group7))
+        self.wait()
         self.play(ShowCreation(gx))
+        self.wait()
         self.play(ShowCreation(gx_label))
+        self.wait()
         self.play(ShowCreation(g_x))
+        self.wait()
         self.play(ShowCreation(g_x_label))
-
-
-
+        self.wait()
+        self.play(Write(line2))
+        self.wait()
+        self.play(Write(dot2))
+        self.wait()
+        self.play(Write(dot2_label))
+        self.wait()
+        group8 = VGroup(gx, gx_label, g_x, g_x_label, line2, dot2, dot2_label)
+        self.play(FadeOut(group8))
+        self.wait()
         
+        s7 = TexMobject(r"{h(x)_{min}} ={1 \over e}").to_corner(UL)
+        s8 = TexMobject(r"{g(x)_{max}} = {1 \over e}").next_to(s7,DOWN)
+        self.play(
+            Write(hx),
+            Write(gx),
+            Write(dot1),
+            Write(dot2),
+            Write(dot1_label),
+            Write(dot2_label)
+        )
+        self.wait()
+        self.play(Write(s7))
+        self.wait()
+        self.play(Write(s8))
+        self.wait()
+        self.play(
+            FadeOut(hx),
+            FadeOut(gx),
+            FadeOut(dot1),
+            FadeOut(dot2),
+            FadeOut(dot1_label),
+            FadeOut(dot2_label),
+            FadeOut(s7),
+            FadeOut(s8),
+            FadeOut(plane),
+            FadeOut(self.axes)
+        )
+        self.wait()
+
+        te1 = TextMobject(r"$h(x)$在$(0,{1\over e})$单调递减,在$({1\over e},+\infty)$单调递增,\\$h(x)$在$(0,+\infty)$的最小值为:${h(x)_{min}}=h({1\over e})={1\over e}$")
+        te2 = TextMobject(r"$g(x)$在$(-\infty, 1)$单调递增,在$(1, +\infty)$单调递减,\\$g(x)$在$(0,+\infty)$的最大值为:${g(x)_{max}}=g(1)={1 \over e}$")
+        te1.to_edge(UP)
+        te2.next_to(te1, DOWN)
+        self.play(
+            Write(te1),
+            Write(te2)
+        )
+        self.wait()
+        te3 = TextMobject(r"综上,当$x>0$时,$h(x)>g(x)$,即$f(x)>1$").move_to(np.array([0,-1.5,0]))
+        self.play(Write(te3))
+        self.wait()
